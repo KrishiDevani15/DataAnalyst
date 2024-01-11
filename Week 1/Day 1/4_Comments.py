44177544a31c91ff2449d3d80eb7e51dc55ad8df
KrishiDevani15 <krishiminergate@gmail.com>
2024-01-11 09:48:52 +0530

    1)Week :- Day 1 Python Keywords and Identifiers

---
+++ Week 1/Day 1/4_Comments.py
@@ -0,0 +1,14 @@
+"""
+4. Comments
+    In the BIG Company we use only Single line comments
+    a) Single-line comment :- #
+    b) Multi-Line comment : Basically there is no multi-line comment but using (3times "") 
+"""
+
+# this is a comment
+# second line
+a = 4
+b = 6 # like this
+
+# second comment
+print(a+b)
---
+++ Week 1/Day 1/5_keywords&identifiers.py
@@ -0,0 +1,22 @@
+"""
+5. Keywords & Identifiers
+    a) Keywords :- there 32 keywords in Python
+    b) Identifiers :- means Create a name for variables,Classes,Objects
+                            1)You can't start with a digit
+                            2) You can use special chars -> _
+                            3) identiers can not be keyword
+Note :- variable is identifier
+"""
+
+
+# Keywords there 32 keywords in Python
+import keyword
+print(keyword.kwlist)
+
+# You can't start with a digit
+name1 = 'Krishi'
+print(name1)
+# You can use special chars -> _
+_ = 'Krishi'
+print(_)
+# identiers can not be keyword
