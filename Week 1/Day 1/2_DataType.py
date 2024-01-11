10d744df05d4fd00d75b39496ea3067d851ccc10
KrishiDevani15 <krishiminergate@gmail.com>
2024-01-11 09:09:47 +0530

    1)Week :- Day 1 Python Data Types

---
+++ Week 1/Day 1/2_DataType.py
@@ -0,0 +1,46 @@
+"""
+2. Data Types : - 9 types of Data Types
+    a) Integer
+    b) Decimal/Float
+    c) Boolean
+    d) Text/String
+    e) complex
+    f) List-> C-> Array
+    g) Tuple
+    h) Sets
+    i) Dictionary
+    j) type
+"""
+
+# Integer
+print(8)
+# 1*10^308
+print(1e308)
+print(1e309)
+
+# Decimal/Float
+print(8.55)
+print(1.7e308)
+print(1.7e309)
+
+# Boolean
+print(True)
+print(False)
+
+# Text/String
+print('Hello World')
+
+# complex
+print(5+6j)
+
+# List-> C-> Array
+print([1,2,3,4,5])
+
+# Tuple
+print((1,2,3,4,5))
+
+# Sets
+print({1,2,3,4,5})
+
+# Dictionary {key : value}
+print({'name':'Krishi','gender':'Male','weight':65})
